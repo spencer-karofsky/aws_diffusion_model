59ece7f2748deffdbe566698124e8d37ef538f1e
Spencer Karofsky <46546340+spencer-karofsky@users.noreply.github.com>
2025-06-28 07:18:47 -0400

    renaming directories for clarity

--- aws_setup/managers/core.py
+++ aws_setup/managers/core.py
@@ -1,4 +1,4 @@
 import boto3
 from aws_setup.utils.logger import logger
-from aws_setup.clients.s3_client import S3BucketInterface, S3ObjectInterface
+from aws_setup.interfaces.s3_client import S3BucketInterface, S3ObjectInterface
 from botocore.exceptions import ClientError
--- aws_setup/tests/core.py
+++ aws_setup/tests/core.py
@@ -1,6 +1,6 @@
 import boto3
 from aws_setup.utils.logger import logger
-from aws_setup.main.s3_main import S3BucketManager, S3ObjectManager
+from aws_setup.managers.s3_main import S3BucketManager, S3ObjectManager
 from botocore.exceptions import ClientError
 import unittest
 from moto import mock_aws
