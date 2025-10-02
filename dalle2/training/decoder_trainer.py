adac76dc1cfbbc28826b7d81df05185fcdfe33a3
Spencer Karofsky <46546340+spencer-karofsky@users.noreply.github.com>
2025-10-02 09:46:16 -0400

    Update decoder_trainer.py

--- dalle2/training/decoder_trainer.py
+++ dalle2/training/decoder_trainer.py
@@ -30,7 +30,7 @@ BATCH_SIZE = 256
 REPEATS = 1  # keep at 1 for the quick test
 
 # Model, optimizer, scheduler
-decoder_model = Decoder(device=device, debug=True, T=1000, img_size=64)
+decoder_model = Decoder(device=device, debug=True, T=1000)
 optimizer = optim.AdamW(decoder_model.parameters(), lr=1e-4)
 noise_scheduler = NoiseScheduler(T=1000, schedule_type='cosine')
 
