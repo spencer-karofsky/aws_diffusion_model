71da14f7a738dda33061871985175c0633d622c8
Spencer Karofsky <46546340+spencer-karofsky@users.noreply.github.com>
2025-10-03 13:36:56 -0400

    Update decoder_trainer.py

--- dalle2/training/decoder_trainer.py
+++ dalle2/training/decoder_trainer.py
@@ -88,5 +88,5 @@ if __name__ == '__main__':
         num_epochs=30,
         save_intermediate_output=50,
         save_intermediate_model=100,
-   #     resume_checkpoint_name='epoch2_batch200.pth'
+        resume_checkpoint_name='final_trained_model.pth'
     )
